bcf8f24e4bddcc58d456f1b0795ced0a89f19e50
Fretard Paul <paulfretard@outlook.com>
2026-06-26 20:07:49 +0200

    Initial Commit

---
+++ Reclaim/ReclaimApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReclaimApp.swift
+//  Reclaim
+//
+//  Created by Paul Fretard on 26/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ReclaimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
